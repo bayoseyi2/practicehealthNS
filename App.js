3af084e5c49bf93a42326f787a11566229ee2a09
bee <bee>
2023-04-27 15:41:11 -0300

    with drawers app js

--- App.js
+++ App.js
@@ -1,95 +1,60 @@
-import React, { createContext, useState, useEffect, useContext } from 'react';
-import { View, ActivityIndicator} from 'react-native';
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from './config/firebase'
-
+import * as React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import Chat from './screens/Chat';
-import Login from './screens/Login';
-import Signup from './screens/Signup';
-import Home from './screens/Home';
-
-
-import {createAppContainer } from 'react-navigation';
-
-import { Container, Content, Header, Body } from 'native-base';
-
-const Stack = createStackNavigator();
-const AuthenticatedUserContext = createContext({});
-
-const AuthenticatedUserProvider = ({children}) => {
-  const [user, setUser] = useState(null);
+function Feed() {
   return (
-    <AuthenticatedUserContext.Provider value={[user, setUser]}>
-      {children}
-    </AuthenticatedUserContext.Provider>
-  )
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Feed Screen</Text>
+    </View>
+  );
 }
 
-function ChatStack () {
+function Notifications() {
   return (
-    <Stack.Navigator defaultScreenOptions={Home}>
-        
-        <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name="Chat" component={Chat}/>
-
-        {/* <Stack.Screen name="Signup" component={Signup}/> */}
-    </Stack.Navigator>
-  )
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications Screen</Text>
+    </View>
+  );
 }
 
-function AuthStack() {
+function Profile() {
   return (
-    <Stack.Navigator defaultScreenOptions={Login} screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Login" component={Login}/>
-        <Stack.Screen name="Signup" component={Signup}/>
-      
-    </Stack.Navigator>
-  )
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile Screen</Text>
+    </View>
+  );
 }
 
-function RootNavigator(){
-  const [user, setUser] = useContext(AuthenticatedUserContext);
-  const [loading, setLoading] = useState(true);
+const Drawer = createDrawerNavigator();
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, 
-      async authenticatedUser =>{
-        authenticatedUser ? setUser(authenticatedUser) : setUser(null);
-        setLoading(false);
-      }
-      );
-      return () => unsubscribe();
-  }, [user]);
-  if(loading) {
-    return(
-      <View style={{flex: 1, justifyContent:'center', alignItems: 'center'}}>
-        <ActivityIndicator size="small"/>
-
-      </View>
-    )
-  }
-
-  return(
-    
-    <NavigationContainer>
-      {user ? <ChatStack/> : <AuthStack/>}
-        
-        
-    </NavigationContainer>
-    
-  )
+function MyDrawer() {
+  return (
+    <Drawer.Navigator useLegacyImplementation initialRouteName="Feed">
+      <Drawer.Screen
+        name="Feed"
+        component={Feed}
+        options={{ drawerLabel: 'Home' }}
+      />
+      <Drawer.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{ drawerLabel: 'Updates' }}
+      />
+      <Drawer.Screen
+        name="Profile"
+        component={Profile}
+        options={{ drawerLabel: 'Profile' }}
+      />
+    </Drawer.Navigator>
+  );
 }
 
 export default function App() {
- return (
-  <AuthenticatedUserProvider>
-      <RootNavigator/>
-  </AuthenticatedUserProvider>
- 
- )
+  return (
+    <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
+  );
 }
-
-
